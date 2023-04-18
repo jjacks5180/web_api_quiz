08a0ef8bef2286ef4a1ccc064841c40a337fa83b
Jeremy Jackson <jjacks5180@gmail.com>
2023-04-17 20:40:17 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let question = [
+  {
+    number: 1,
+    question: "what can javascript do?",
+    answers: [
+      "create a website",
+      "change html",
+      "create a database",
+      "all of the above",
+    ],
+    correctAnswer: "all of the above",
+  },
+  {
+    numbers: 2,
+    question: "which one is the correct data type?",
+    answers: ["string", "methods", "innerHTML", "let"],
+    correctAnswer: "string",
+  },
+  {
+    number: 3,
+    question: "which example best shows what a boolean is?",
+    answers: ["yellow", "true", "car, truck, bike", "17.5"],
+    correctAnswer: "true",
+  },
+  {
+    number: 4,
+    question:
+      "which example shows the command to declare a variable in javascript ?",
+    answers: ["value", "x = 5", "const", "all of the above"],
+    correctAnswer: "const",
+  },
+];
